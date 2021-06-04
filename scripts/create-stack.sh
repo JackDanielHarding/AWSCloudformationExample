f0bbb036d0a3d6754e9d4ee70cf075894b1df954
jackdanielharding <jackdanielharding@gmail.com>
2021-06-04 01:59:22 +0100

    initial commit

---
+++ scripts/create-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name UdacityIaCProjectStack --template-body file://network.yml --parameters file://parameters.json --region us-west-2
---
+++ scripts/update-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name UdacityIaCProjectStack --template-body file://network.yml --parameters file://parameters.json  --region us-west-2
